460f0f88f058e22527e59c4d570f25ab6fa924de
Berkkenz <155427440+Berkkenz@users.noreply.github.com>
2024-01-13 07:12:55 -0500

    Update forgefile.bat

--- Content/Exe/forgefile.bat
+++ Content/Exe/forgefile.bat
@@ -4,10 +4,10 @@ setlocal
 echo %cd%
 pause
 
-cd /d "%~dp0\Content\Exe"
+cd "%~dp0\Content\Exe"
 java -jar forge-1.19.2-43.3.7-installer.jar
 if %errorlevel% neq 0 (
 	echo Forge failed to install.
 	pause
 	exit /b 1
-)
+)d

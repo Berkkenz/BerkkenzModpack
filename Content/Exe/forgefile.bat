404fd672a842c395a180b3e7150f0416767d4f9d
Berkkenz <155427440+Berkkenz@users.noreply.github.com>
2024-01-13 07:09:18 -0500

    1

--- Content/Exe/forgefile.bat
+++ Content/Exe/forgefile.bat
@@ -4,7 +4,7 @@ setlocal
 echo %cd%
 pause
 
-java -jar "%~dp0\Content\Folders\Exe\forge-1.19.2-43.3.7-installer.jar" --installClient
+java -jar "%~dp0\Content\Folders\Exe\forge-1.19.2-43.3.7-installer.jar"
 if %errorlevel% neq 0 (
 	echo Forge failed to install.
 	pause
